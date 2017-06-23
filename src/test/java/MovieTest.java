b73d8c01b6b13ffa3c5e4b9adde21aaeffe16fff
Jake <kc80.lee@samsung.com>
2017-06-23 17:29:53 +0900

    테스트코드 리팩토링(코드중복 제거) extract field, Initialize in: setup 선택 @Before가 만들어짐.

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -1,18 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MovieTest {
+
+    private Movie movie;
+
+    @Before
+    public void setUp() throws Exception {
+        movie = new Movie();
+    }
+
     @Test
     public void should_return_0_when_just_created() {
-        Movie movie = new Movie();
         assertThat(movie.averageRating(), is(0));
     }
 
     @Test
     public void should_return_1_when_1_was_rated() {
-        Movie movie = new Movie();
         movie.rate(1);
         assertThat(movie.averageRating(), is(1));
     }
